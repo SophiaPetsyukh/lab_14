a3c6de926e4d3f516073f337b00bd0e5815a2e55
SophiaPetsyukh <47388433+SophiaPetsyukh@users.noreply.github.com>
2019-05-25 00:07:46 +0300

    Update search_time_test.py

--- task 2/search_time_test.py
+++ task 2/search_time_test.py
@@ -1,20 +1,52 @@
 from random import choice
+from task1.linkedbst import LinkedBST
+from time import time
 
-def open(filename):
+def read(filename):
     with open(filename, encoding='utf-8') as f:
         data = []
         for el in f.readlines():
             data.append(el.strip())
     return data
 
-def get_random_1000():
+def get_random_10000():
     result = []
-    data = open('words.txt')
+    data = read('words.txt')
     for i in range(10000):
         word = choice(data)
         result.append(word)
         data.remove(word)
     return result
 
-def search_lst():
-    pass
+def search_lst(dictionary, words):
+    result = []
+    for word in dictionary:
+        if word in words:
+            result.append(word)
+    return result
+
+def build_tree(data):
+    tree = LinkedBST()
+    for word in data:
+        tree.add(word)
+    return tree
+
+def search_tree(tree, words):
+    result = []
+    for el in words:
+        if tree.__contains__(el):
+            result.append(el)
+    return result
+
+if __name__ == '__main__':
+    words = get_random_10000()
+    dictionary = read('words.txt')
+    tree = build_tree(dictionary)
+
+    start1 = time()
+    search_tree(tree, words)
+    print(time() - start1)
+    start2 = time()
+    search_lst(dictionary, words)
+    print(time() - start2)
+    
